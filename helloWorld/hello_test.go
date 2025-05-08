dfc2cfe174ffadf06492a30e8399431cc65a0c31
Daniel Tineli jr <danieltineli.td@gmail.com>
2025-05-07 21:42:25 -0300

    refactor

--- helloWorld/hello_test.go
+++ helloWorld/hello_test.go
@@ -3,11 +3,24 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+	t.Run("Saying Hello to people", func(t *testing.T) {
+		want := "Hello, Tineli"
 
-	want := "Hello, Tineli"
+		got := Hello("Tineli")
 
-	got := Hello("Tineli")
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("Should return Hello, world as default", func(t *testing.T) {
+		want := "Hello, world"
+		got := Hello("")
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got: %q want %q", got, want)
 	}
